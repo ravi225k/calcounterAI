c55eacf2541aac4d5ba8e2cd696403b18e039622
ravi kanth <ravi225k@gmail.com>
2025-11-05 05:50:40 +0000

    when I am saving this as an app on my mobile I am getting firebase icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#333333',
+  themeColor: '#000000',
 }
 
 export default function RootLayout({
@@ -23,6 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
       </head>
       <body className="font-body antialiased">
         {children}
